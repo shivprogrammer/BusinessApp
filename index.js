151c5b1da1ba7c32d96d93ffc26fad08ec9b8b65
shivprogrammer <shiv.unadkat@gmail.com>
2017-12-01 03:23:36 -0800

    switch is successfully working [iOS WORKING]

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ import {
   AppRegistry,
   View,
   Image,
+  Text,
   ScrollView,
   Switch
 } from 'react-native';
 import Home from './app/components/Home/Home';
 import About from './app/components/About/About';
 import Contact from './app/components/Contact/Contact';
-
 const styles = require('./app/style');
 
 export default class ShivvyBusinessApp extends Component {
@@ -21,10 +21,18 @@ export default class ShivvyBusinessApp extends Component {
   }
 
   render() {
+    let logoHidden = this.state.logoHidden;
+    let img = <Text />;
+    if (logoHidden) {
+      img = <Text />;
+    } else {
+      img = <Image style={styles.image} source={require('./logo.png')} />
+    }
+
     return (
       <View>
         <View style={styles.imageWrap}>
-          <Image style={styles.image} source={require('./logo.png')} />
+          {img}
           <Switch onValueChange={(value) => this.setState({ logoHidden: value })} value={this.state.logoHidden} />
         </View>
 
